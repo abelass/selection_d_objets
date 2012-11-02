d7a5d722fa23280815752ccdf5c35342c858395c
abelass <websolutions@mychacra.net>
2012-11-02 15:19:07 +0100

    Définition

--- lang/paquet-selection_objet_fr.php
+++ lang/paquet-selection_objet_fr.php
@@ -6,7 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'selection_objet_description' => 'Permet de séléctionner des objets spip, de les ranger et de perselection_objetnaliser leur affichage selon la rubrique choisie.',
+	'selection_objet_description' => 'Plugin inscpiré du plugin séléction d\'aticvle. 
+	Permet de séléctionner des objets spip, de les ranger et de personnaliser leur affichage selon la rubrique choisie.',
 	'selection_objet_nom' => 'Selection d\'objets',
 	'selection_objet_slogan' => 'Listez vos objets spips',
 );
